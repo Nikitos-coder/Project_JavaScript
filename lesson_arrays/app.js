79eb507b20a867f4617cec62dd0f866c5a74f5f6
Nikitos <buryhnikita@gmail.com>
2021-07-05 14:59:15 +0300

    add lesson

---
+++ lesson_arrays/app.js
@@ -0,0 +1,116 @@
+const div = document.querySelector("div");
+const titles = document.querySelectorAll("h1");
+const btn = document.querySelector('.btn')
+// const h1 = document.getElementsByTagName('h1');
+// console.dir(h1);
+// console.log(titles);
+// console.log(Array.from(titles));
+// console.log(Array.prototype.slice.call(titles));
+// console.log([...titles]);
+
+// console.log(div)
+
+// const link = div.querySelector('.link');
+// console.log(link.parentElement)
+// console.log(link.closest('.content'));
+
+// div.classList.add('acrticle', 'custom')
+// div.classList.remove('custom')
+
+// btn.addEventListener('onclick', func)
+
+// function func() {
+//     let x = document.getElementById('title')
+//     if (x.style.display == 'none'){ 
+//         x.style.display = 'block'
+//     }else {
+//         x.style.display = 'none'
+//     }
+// }
+
+// btn.addEventListener('click', button => {
+//     button.toggle(title.parentElement.removeChild(title))
+// })
+
+// div.setAttribute('id', 'myId')
+// // Manipulation DOM
+// const title = document.querySelector("h1")
+
+
+// title.innerHTML = '<span>Text for inner</span>'
+// title.textContent = 'new text'
+// // title.appendChild()
+// title.insertAdjacentHTML
+// title.insertAdjacentElement('<h2>Tutle</h2>')
+// title.innerHTML += '<span>New text</span>'
+// const span = title.querySelector('span')
+// console.log(span)
+
+// *Create element
+// const span = document.createElement('span')
+// span.textContent = 'span created by create element'
+// span.classList.add('specialClass')
+// title.appendChild(span)
+// // div.after(span)
+
+
+// const fragment = document.createDocumentFragment();
+// colors = ['green', 'yellow', 'black']
+// colored = ['purple', 'red', 'blue']
+
+// colors.forEach(color => {
+//     const item = document.createElement('div')
+//     // item.classList.add(color)
+//     item.style.background = color
+//     item.textContent = color
+//     fragment.appendChild(item)
+// })
+// document.body.before(fragment)
+
+// title.parentElement.removeChild(title)
+// Delete elements
+
+ 
+
+// var arr = ['1', '2', '3', '4', '5', '6', '7', '8'];
+// arr[0] = "Jani";
+// arr[1] = "Hege";
+// arr[2] = "Stale";
+// arr[3] = "Kai Jim";
+// arr[4] = "Borge";
+// arr.splice(6, 1, "00000");
+// console.log(arr.join());
+// arr.splice(0, 1, "111111");
+// console.log(arr.join());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
